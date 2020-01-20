d7737269108f898dce90eb547108e10df31e014e
Rafael Reyes <r.reyes@caribemedia.com.do>
2020-01-20 12:00:08 -0400

    Version two of rarepz.com

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo-rareyes.png';
+import Header from './Components/Header/Header';
+import TextHeader from './Components/TextHeader/TextHeader';
 import './App.css';
-
+// import { facebook, faInstagram } from '@fortawesome/free-solid-svg-icons';
+import facebook from './facebook.svg';
+import instagram from './instagram.svg';
+// import  { TweenMax, Power3 } from 'gsap';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const redes = [
+    {
+      icon: facebook,
+      link: 'https://www.facebook.com/Rafreye',
+      target: '_blank',
+      rel: 'noopener noreferrer”',
+      id: 'facebook'
+    },
+    {
+      icon: instagram,
+      link: 'https://www.instagram.com/alexander_repz/',
+      target: '_blank',
+      rel: 'noopener noreferrer”',
+      id: 'instagram'
+    }
+  ];
+
+  const red = redes.map((red) =>{
+    return (
+      <li key={ red.id }><a href={ red.link } target={ red.target } rel={ red.rel } ><img src={red.icon} alt={red.id} /></a></li>
+    )
+  });
+    return(
+      <div className="App">
+        {/* ======= LOGO ====== */}
+        <div className="logo-area">
+            <img src={logo} alt='logo-rareyes' className="logo" />
+        </div>
+
+        {/* ======= CONTENT ====== */}
+        <div className="container">
+          <div className="graisss">
+            <Header />
+          </div>
+          <TextHeader />
+        </div>
+
+
+        {/* ======= REDES ====== */}
+        <div className='redes'>
+          <ul className="redes-sociales">
+            {red}
+          </ul>
+        </div>
+        {/* ======= REDES ====== */}
+
+        <div className="footer-space"></div>
+      </div>
+    )
 }
 
 export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import  { TweenMax, Power3 } from 'gsap';
+import './Header.css';
+// import rafael from './images/rafaelreyes.svg'
+
+const Header = () =>{
+  return (
+    <div className="Header">
+        <div className="title-header">
+            <h1>RAFAEL </h1>
+            <h1 className="text-flex"><span>FRONT-END</span> REYES <span>DEVELOPER</span></h1>
+        </div>
+    </div>
+  );
+}
+
+export default Header;
